15eb8278816e6c60e252c99a181f35dd5d59106a
boobish@gmail.com <doufish123!>
2021-02-13 12:48:53 +0200

    Restructuring to use openzeppelin libs properly

--- Solidity/migrations/4_deploy_contract.js
+++ Solidity/migrations/4_deploy_contract.js
@@ -1,6 +1,7 @@
 const RoomLockSimulator = artifacts.require("Simulation/RoomLockSimulator.sol");
 const SafeERC20 = artifacts.require("openzeppelin/contracts/token/ERC20/SafeERC20.sol");
 
+
 module.exports = function(deployer) {
     deployer.deploy(SafeERC20);
     deployer.deploy(RoomLockSimulator);
