c43d14abde2d041cc169e5fcf82b9e583a0bda31
Tareq doufish <boobish@gmail.comd>
2021-02-16 22:06:32 +0200

    Typos and organization

--- Solidity/contracts/NFT/NFTStake.sol
+++ Solidity/contracts/NFT/NFTStake.sol
@@ -6,58 +6,54 @@ import "./ERC1155.sol";
 import "./IERC20.sol";
 
 
-contract  NFTStake is IERC1155Receiver{
+contract NFTStake is IERC1155Receiver {
+
     using SafeMath for uint256;
-    
+
     IERC20  roomToken = IERC20(0xdDF0667c0694d1AEbED930E30ea06b69BB0D868E);  // correct 0xAd4f86a25bbc20FfB751f2FAC312A0B4d8F88c64
-    
+
     // todo: set the correct ROOm NFT
-    IERC1155 NFTToken = IERC1155(0x40c45a58aeFF1c55Bd268e1c0b3fdaFD1E33CDf0) ;
-    
+    IERC1155 NFTToken = IERC1155(0x40c45a58aeFF1c55Bd268e1c0b3fdaFD1E33CDf0);
+
     uint256 _finishBlock;
     address private _roomTokenRewardsReservoirAddress;
-    
-    mapping (uint256 => mapping(address => bool)) nftLockedToStakeRoom;
-    
-    mapping (uint256 => uint256) private _totalSupplay;
-    mapping (uint256 => mapping(address => uint256)) private _balances;
-    
-    mapping (uint256 => uint256) _lastUpdateBlock;
-    mapping (uint256 => uint256) _accRewardPerToken;
-    mapping (uint256 => uint256) _rewardPerBlock;
-    
-    mapping (uint256 => mapping(address => uint256)) private _prevAccRewardPerToken; // previous accumulative reward per token (for a user)
-    mapping (uint256 => mapping(address => uint256)) private _rewards; // rewards balances 
-   
+    address private _owner;
+
+    mapping(uint256 => mapping(address => bool)) nftLockedToStakeRoom;
+
+    mapping(uint256 => uint256) private _totalSupplay;
+    mapping(uint256 => mapping(address => uint256)) private _balances;
+
+    mapping(uint256 => uint256) _lastUpdateBlock;
+    mapping(uint256 => uint256) _accRewardPerToken;
+    mapping(uint256 => uint256) _rewardPerBlock;
+
+    mapping(uint256 => mapping(address => uint256)) private _prevAccRewardPerToken; // previous accumulative reward per token (for a user)
+    mapping(uint256 => mapping(address => uint256)) private _rewards; // rewards balances
+
     event Staked(uint256 poolId, address indexed user, uint256 amount);
     event Unstaked(uint256 poolId, address indexed user, uint256 amount);
     event ClaimReward(uint256 poolId, address indexed user, uint256 reward);
     event RoomTokenWalletEmpty();
-    
-    address private _owner;
-    
-    function onERC1155Received(address, address, uint256, uint256, bytes calldata) external override returns(bytes4) {
+
+    function onERC1155Received(address, address, uint256, uint256, bytes calldata) external override returns (bytes4) {
         return this.onERC1155Received.selector;
     }
-	
-	function onERC1155BatchReceived(address, address, uint256[] calldata, uint256[] calldata, bytes calldata) external override returns(bytes4) {
-		return this.onERC1155BatchReceived.selector;
-	}
-	
-	function supportsInterface(bytes4) external override view returns (bool) {
-		return true;
-	}
-    
-        
+
+    function onERC1155BatchReceived(address, address, uint256[] calldata, uint256[] calldata, bytes calldata) external override returns (bytes4) {
+        return this.onERC1155BatchReceived.selector;
+    }
+
+    function supportsInterface(bytes4) external override view returns (bool) {
+        return true;
+    }
+
     constructor () public {
-
         _owner = msg.sender;
-
-
         _rewardPerBlock[0] = 0;
-        
+
         // for math precision
-        
+
         // TODO: calculate finish block
         _finishBlock = 0;
         _rewardPerBlock[0] = blockNumber();
@@ -72,28 +68,28 @@ contract  NFTStake is IERC1155Receiver{
         _lastUpdateBlock[3] = blockNumber();
         _lastUpdateBlock[4] = blockNumber();
     }
-    
-    function stake(uint256 poolId, uint256 amount) public{
+
+    function stake(uint256 poolId, uint256 amount) public {
         updateReward(poolId, msg.sender);
-        
-        if(amount > 0){
-            if(nftLockedToStakeRoom[poolId][msg.sender] == false){
+
+        if (amount > 0) {
+            if (nftLockedToStakeRoom[poolId][msg.sender] == false) {
                 nftLockedToStakeRoom[poolId][msg.sender] = true;
-                NFTToken.safeTransferFrom(msg.sender,address(this), poolId, 1 , "");
+                NFTToken.safeTransferFrom(msg.sender, address(this), poolId, 1, "");
             }
-            
+
             _totalSupplay[poolId] = _totalSupplay[poolId].add(amount);
             _balances[poolId][msg.sender] = _balances[poolId][msg.sender].add(amount);
-            
+
             roomToken.transferFrom(msg.sender, address(this), amount);
-            
+
             emit Staked(poolId, msg.sender, amount);
         }
     }
-    
-    function unstake(uint256 poolId, uint256 amount, bool claim) public{
+
+    function unstake(uint256 poolId, uint256 amount, bool claim) public {
         updateReward(poolId, msg.sender);
-        
+
         if (amount > 0) {
             _totalSupplay[poolId] = _totalSupplay[poolId].sub(amount);
             _balances[poolId][msg.sender] = _balances[poolId][msg.sender].sub(amount);
@@ -101,70 +97,70 @@ contract  NFTStake is IERC1155Receiver{
             roomToken.transfer(msg.sender, amount);
             emit Unstaked(poolId, msg.sender, amount);
         }
-        
+
         if (claim) {
-              uint256 reward = _rewards[poolId][msg.sender];
-              
-              uint256 walletBalanace = roomToken.balanceOf(_roomTokenRewardsReservoirAddress);
-              
-              if (reward > 0 && walletBalanace > reward) {
+            uint256 reward = _rewards[poolId][msg.sender];
+
+            uint256 walletBalanace = roomToken.balanceOf(_roomTokenRewardsReservoirAddress);
+
+            if (reward > 0 && walletBalanace > reward) {
                 _rewards[poolId][msg.sender] = 0;
                 // Instead of minting we transfer from this contract address to the message sender.
                 roomToken.transferFrom(_roomTokenRewardsReservoirAddress, msg.sender, reward);
                 emit ClaimReward(poolId, msg.sender, reward);
             }
-         }
-    }
-    
-    function exit(uint256 poolId) public{
-        unstake(poolId, _balances[poolId][msg.sender], true);
-        if(nftLockedToStakeRoom[poolId][msg.sender]){
-            nftLockedToStakeRoom[poolId][msg.sender] = false;
-            NFTToken.safeTransferFrom(address(this), msg.sender, poolId, 1 , "");
         }
     }
-    
-    function claimReward(uint256 poolId) public returns ( uint256 reward, uint8 reason) {
-        updateReward(poolId,msg.sender);
+
+    function exit(uint256 poolId) public {
+        unstake(poolId, _balances[poolId][msg.sender], true);
+        if (nftLockedToStakeRoom[poolId][msg.sender]) {
+            nftLockedToStakeRoom[poolId][msg.sender] = false;
+            NFTToken.safeTransferFrom(address(this), msg.sender, poolId, 1, "");
+        }
+    }
+
+    function claimReward(uint256 poolId) public returns (uint256 reward, uint8 reason) {
+        updateReward(poolId, msg.sender);
         // 0 means successful operation for transferring tokens.
         reason = 0;
         reward = _rewards[poolId][msg.sender];
 
         // TODO: chose if or require
         if (reward > 0) {
-            
+
             uint256 walletBalanace = roomToken.balanceOf(_roomTokenRewardsReservoirAddress);
             if (walletBalanace < reward) {
                 // 1 means the wallet is empty.
                 reason = 1;
                 emit RoomTokenWalletEmpty();
-            }else{
+            } else {
                 _rewards[poolId][msg.sender] = 0;
                 roomToken.transferFrom(_roomTokenRewardsReservoirAddress, msg.sender, reward);
-                emit ClaimReward(poolId,msg.sender, reward);
+                emit ClaimReward(poolId, msg.sender, reward);
             }
         }
         return (reward, reason);
     }
-    
-    function updateReward(uint256 poolId, address account) public{
+
+    function updateReward(uint256 poolId, address account) public {
         // reward algorithm
         // in general: rewards = (reward per token ber block) user balances
         uint256 cnBlock = blockNumber();
-        
+
         // update accRewardPerToken, in case totalSupply is zero; do not increment accRewardPerToken
         if (_totalSupplay[poolId] > 0) {
-            
-             uint256 lastRewardBlock = cnBlock < _finishBlock ? cnBlock : _finishBlock;
-             if (lastRewardBlock > _lastUpdateBlock[poolId]) {
+
+            uint256 lastRewardBlock = cnBlock < _finishBlock ? cnBlock : _finishBlock;
+            if (lastRewardBlock > _lastUpdateBlock[poolId]) {
                 _accRewardPerToken[poolId] = lastRewardBlock.sub(_lastUpdateBlock[poolId])
                 .mul(_rewardPerBlock[poolId]).div(_totalSupplay[poolId])
                 .add(_accRewardPerToken[poolId]);
             }
         }
-        
+
         _lastUpdateBlock[poolId] = cnBlock;
-        
+
         if (account != address(0)) {
 
             uint256 accRewardPerTokenForUser = _accRewardPerToken[poolId].sub(_prevAccRewardPerToken[poolId][account]);
@@ -180,12 +176,12 @@ contract  NFTStake is IERC1155Receiver{
             }
         }
     }
-    
-     function rewards(uint256 poolId, address account) external view returns (uint256 reward) {
+
+    function rewards(uint256 poolId, address account) external view returns (uint256 reward) {
         // read version of update
         uint256 cnBlock = blockNumber();
         uint256 accRewardPerToken = _accRewardPerToken[poolId];
-        
+
         // update accRewardPerToken, in case totalSupply is zero; do not increment accRewardPerToken
         if (totalSupply(poolId) > 0) {
             uint256 lastRewardBlock = cnBlock < _finishBlock ? cnBlock : _finishBlock;
@@ -201,19 +197,19 @@ contract  NFTStake is IERC1155Receiver{
         .div(1e18)
         .add(_rewards[poolId][account]);
     }
-    
-    function totalSupply(uint256 poolId) public view returns(uint256){
+
+    function totalSupply(uint256 poolId) public view returns (uint256){
         return _totalSupplay[poolId];
     }
-    
-    function balanceOf(uint256 poolId, address account) public view returns(uint256){
+
+    function balanceOf(uint256 poolId, address account) public view returns (uint256) {
         return _balances[poolId][account];
     }
-    
-    
+
+
     function blockNumber() public view returns (uint256) {
         return block.number;
     }
-    
-    
+
+
 }
--- Solidity/contracts/NFT/RoomNFT.sol
+++ Solidity/contracts/NFT/RoomNFT.sol
@@ -5,112 +5,117 @@ pragma solidity ^0.6.0;
 import "./ERC1155.sol";
 import "./IERC20.sol";
 
-contract RoomNFT is ERC1155{
-   
-   
-    mapping (uint256 => uint256) private _totalSupplay;
-    mapping (uint256 => uint256) private _capital;
-    mapping (uint256 => uint256) public requiredRoomBurned;
-    
+// This is OptionNFT
+contract RoomNFT is ERC1155 {
+
+    mapping(uint256 => uint256) private _totalSupplay;
+    mapping(uint256 => uint256) private _capital;
+    mapping(uint256 => uint256) public requiredRoomBurned;
+
     uint256 constant TIER1 = 0;
     uint256 constant TIER2 = 1;
     uint256 constant TIER3 = 2;
     uint256 constant TIER4 = 3;
     uint256 constant TIER5 = 4;
-    
+
     //TODO: ROOM: 0xAd4f86a25bbc20FfB751f2FAC312A0B4d8F88c64
     //IERC20  roomToken = IERC20(0xAd4f86a25bbc20FfB751f2FAC312A0B4d8F88c64);
     IERC20  roomToken = IERC20(0xdDF0667c0694d1AEbED930E30ea06b69BB0D868E);
-    
+
     address constant roomBurnAdd = address(0x000000000000000000000000000000000000dEaD);
-    
+
     constructor() public ERC1155("uri"){
-            _capital[TIER1] = 50;
-            _capital[TIER2] = 40;
-            _capital[TIER3] = 30;
-            _capital[TIER4] = 20;
-            _capital[TIER5] = 8;
-            
-            requiredRoomBurned[TIER1] = 500e18;
-            requiredRoomBurned[TIER2] = 120e18; // + burn 1 tire1
-            requiredRoomBurned[TIER3] = 120e18; // + burn 1 tire2
-            requiredRoomBurned[TIER4] = 120e18; // + burn 1 tire3
-            requiredRoomBurned[TIER5] = 160e18; // + burn 1 tire4
+        _capital[TIER1] = 50;
+        _capital[TIER2] = 40;
+        _capital[TIER3] = 30;
+        _capital[TIER4] = 20;
+        _capital[TIER5] = 8;
+
+        requiredRoomBurned[TIER1] = 500e18;
+        requiredRoomBurned[TIER2] = 120e18;
+        // + burn 1 tire1
+        requiredRoomBurned[TIER3] = 120e18;
+        // + burn 1 tire2
+        requiredRoomBurned[TIER4] = 120e18;
+        // + burn 1 tire3
+        requiredRoomBurned[TIER5] = 160e18;
+        // + burn 1 tire4
     }
-    
+
     function burn(address account, uint256 id, uint256 value) internal virtual {
         require(
             account == _msgSender() || isApprovedForAll(account, _msgSender()),
             "ERC1155: caller is not owner nor approved"
         );
-        
+
         _totalSupplay[id] = _totalSupplay[id].sub(value);
         _burn(account, id, value);
     }
-    
+
     function mint(address account, uint256 id, uint256 amount, bytes memory data) internal {
-        
-        uint256 newTotalSupply =  _totalSupplay[id].add(amount);
-        require( newTotalSupply <= _capital[id] ,"total supply exceeds capital");
+
+        uint256 newTotalSupply = _totalSupplay[id].add(amount);
+        require(newTotalSupply <= _capital[id], "total supply exceeds capital");
         _totalSupplay[id] = newTotalSupply;
-        
-        _mint(account,id,amount,data);
+
+        _mint(account, id, amount, data);
     }
-    
-    function totalSupply(uint256 id) public view returns(uint256){
+
+    function totalSupply(uint256 id) public view returns (uint256) {
         return _totalSupplay[id];
     }
-    
-    function capital(uint256 id) public view returns(uint256){
+
+    function capital(uint256 id) public view returns (uint256) {
         return _capital[id];
     }
-    
-    function CheckAvilableToMint(uint256 id) public view returns(uint256){
+
+    function CheckAvilableToMint(uint256 id) public view returns (uint256) {
         return _capital[id].sub(_totalSupplay[id]);
     }
-    
-    
-    function mintTier(uint256 id) public{
-        require (id <5,"unsupported id");
-        
-        if(id >0 ){
-            burn(_msgSender(),id.sub(1),1); // burn previous tier
+
+
+    function mintTier(uint256 id) public {
+        require(id < 5, "unsupported id");
+
+        if (id > 0) {
+            burn(_msgSender(), id.sub(1), 1);
+            // burn previous tier
         }
-        
+
         roomToken.transferFrom(_msgSender(), roomBurnAdd, requiredRoomBurned[id]);
         mint(_msgSender(), id, 1, "");
     }
-    
-    
-    function CheckCanMint(address account,uint256 id) public view returns(bool canMint, string memory reason){
+
+
+    function CheckCanMint(address account, uint256 id) public view returns (bool canMint, string memory reason){
         uint256 avilableToMint = CheckAvilableToMint(id);
-        if(avilableToMint == 0){
+        if (avilableToMint == 0) {
             canMint = false;
             reason = "no available kkkk";
-            return (canMint,reason);
+            return (canMint, reason);
         }
-        
-        if(id > 0){
-            if(balanceOf(account,id.sub(1)) == 0 ){
+
+        if (id > 0) {
+            if (balanceOf(account, id.sub(1)) == 0) {
                 canMint = false;
                 reason = "account has no previous tier";
-                return (canMint,reason);
+                return (canMint, reason);
             }
-            
-            if(!isApprovedForAll(account,address(this))){
+
+            if (!isApprovedForAll(account, address(this))) {
                 canMint = false;
                 reason = "account dosnot approve this";
-                return (canMint,reason);
+                return (canMint, reason);
             }
         }
-        
-        if(roomToken.balanceOf(account) < requiredRoomBurned[id]){
+
+        if (roomToken.balanceOf(account) < requiredRoomBurned[id]) {
             canMint = false;
             reason = "account has not the required ROOM amount";
-            return (canMint,reason);
+            return (canMint, reason);
         }
-        
+
         canMint = true;
-        
+
     }
 }
