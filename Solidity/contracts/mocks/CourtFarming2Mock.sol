d31826c20198699e9e8b878ff1d72cee09dba978
Tareq doufish <boobish@gmail.comd>
2021-03-06 21:17:35 +0200

    Adding a new mock. Adding test cases to the new contract.

---
+++ Solidity/contracts/mocks/CourtFarming2Mock.sol
@@ -0,0 +1,48 @@
+pragma solidity ^0.5.0;
+
+import "../Farming/CourtFarming2.sol";
+
+contract CourtFarming2Mock is CourtFarming {
+
+
+//    // TODO: fill this info Those are the on the contract deployed.
+//    uint256 totalRewards  = 45000e18;
+//    uint256 rewardsPeriodInDays = 450;
+//    uint256 incvTotalRewards = 18000e18;
+//    uint256 incvRewardsPeriodInDays = 60;
+//incvLockTime = 1640995200; // 01/01/2022
+
+//    uint256 totalRewards = (5760 * 450) * 1e18;
+//    uint256 rewardsPeriodInDays = 1 * 450;
+    uint256 incvTotalRewards = (5760 * 450) * 1e18;
+    uint256  incvRewardsPeriodInDays = 1 * 450;
+    uint256 incvLockTimeAssigned = 1640995200; // 01/01/2022
+
+//incvTotalRewards, uint256 incvRewardsPeriodInDays
+    constructor() public CourtFarming(incvTotalRewards, incvRewardsPeriodInDays) {
+        // Taken from the testing deployed
+        incvStartReleasingTime = 1640995200; // 01/01/2022 // check https://www.epochconverter.com/ for timestamp
+        incvBatchPeriod = 1 days;
+        incvBatchCount = 1;
+    }
+
+    function changeToContractAttributes() public {
+        changeStakeParameters(18000e18, 60, 1640995200);
+    }
+
+    function setLPToken(address stakingAddress) public {
+        stakedToken = IERC20(address(stakingAddress));
+    }
+
+    function setStakingToken(address courtStakeAdd) public {
+        courtToken = IMERC20(address(courtStakeAdd));
+    }
+
+    function getCurrentTime() public view returns (uint256){
+        return block.timestamp;
+    }
+
+    function getIncReleaseTime() public view returns (uint256) {
+        return incvStartReleasingTime;
+    }
+}
